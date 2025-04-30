a8330a8197524a2e20f916d0ec5f507d63bf258e
alperenTurkseven <turksevenalperen0@gmail.com>
2025-04-30 15:18:50 +0300

    client tabanlı giriş değişti

---
+++ app/job-posts/JobPostsServer.tsx
@@ -0,0 +1,19 @@
+// app/job-posts/JobPostsServer.tsx
+import { prisma } from '@/lib/prisma'
+import JobPostsClient from '@/components/JobPostsClient'
+
+export default async function JobPostsServer() {
+  const jobPosts = (await prisma.jobPost.findMany({
+    include: {
+      author: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })).map(post => ({
+    ...post,
+    createdAt: post.createdAt.toISOString(),
+  }))
+
+  return <JobPostsClient initialPosts={jobPosts} />
+}
