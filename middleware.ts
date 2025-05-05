39c40568dd419959c8b828dbe0325d86689dba94
alperenTurkseven <turksevenalperen0@gmail.com>
2025-05-05 10:45:59 +0300

    login giriş

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET })
 
   if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url))
+    return NextResponse.redirect(new URL("/", req.url))
   }
 
   return NextResponse.next()
